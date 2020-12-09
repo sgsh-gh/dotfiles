fa30c71627e7558193060c47b981f40838a77609
sgsh <s.sugano@sgsh.com>
2020-12-09 09:06:19 +0900

    Add old python version

--- bin/pyenv.sh
+++ bin/pyenv.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")" || exit
 pyenv --version  >/dev/null 2>&1 || exit
 
 # TODO: investigate error for 3.6.12
-l='3.7.9 3.8.6 3.9.0'
+l='3.6.8 3.7.9 3.8.6 3.9.0'
 for v in $l; do
   echo "Installing Python$v..."
   CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" \
