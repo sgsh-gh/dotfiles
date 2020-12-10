7b0b7daf3587ccfc106b3647151609e8ff9cd7d4
sgsh <s.sugano@sgsh.com>
2020-12-10 10:00:56 +0900

    Add secret decode function Add arc rebase function Add brew rollback function add k8s function

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -46,6 +46,8 @@ function kctl() {
       echo "Undefined" ;;
     'detached_nwp' )
       echo "Undefined" ;;
+    'decode_sec' )
+      kubectl get secret "$1" -ojson | jq -r '.data[]' | base64 -D ;;
     * )
       kubectl "$@"
   esac
@@ -63,3 +65,34 @@ function zm() {
     open "zoommtg://zoom.us/join?confno=$1&pwd=$2"
   fi
 }
+###
+
+### brew
+function brew_rollback() {
+  if [ -n "$1" ] && [ -n "$2" ]; then
+    cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core/Formula"
+    brew unlink "$1"
+#    git log --grep="$1 $2" --pretty=%H | xargs -IX git checkout X -- "$1.rb"
+#    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+    h=$(git log --grep="$1 $2" --pretty=%H)
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install "https://raw.githubusercontent.com/Homebrew/homebrew-core/$h/Formula/$1.rb"
+
+    git reset --hard
+    cd -
+  fi
+}
+###
+
+### Phablicity
+#function arc_rebase() {
+#   d=`arc diff master --allow-untracked  -m 'Stashed current changes' | grep ' Revision URI:' | awk -F '/' '{print $4}'`
+#
+#  build-support/bin/git_patch_trunk.sh
+#
+#  echo "You update $d"
+#  read -p "ok? (y/N): " yn
+#  case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+#
+#  arc diff master --allow-untracked -m 'Patched current master' -update $d
+#}
+###
